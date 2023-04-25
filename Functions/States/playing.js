8b3eb09a6247e6b1c18276af9efc9a4afbb699fd
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-04-25 13:21:09 -0400

    Add comments and update music files

--- Functions/States/playing.js
+++ Functions/States/playing.js
@@ -1,66 +1,70 @@
+// Import necessary modules
 const { getAlbumArt } = require("../Images/getAlbumArt.js");
 const { searchShow } = require("../Images/searchShow.js");
 const config = require("../../Storage/config.js");
 
+// Define the async function 'play'
 async function play(status) {
+  // Initialize variables
   state = "Playing";
   var details = "";
+
+  // Extract information about what's playing
   const meta = status.information.category.meta;
 
-  if (status.stats.decodedvideo > 0) {
-    // If it's a tv show
+  // If the video has a decoded video stream
+  if (status.stats.decodedvideo > 0 && !meta.artist) {
+    // If it's a TV show
     if (meta.showName) {
+      // Set the details variable to the name of the show
       var details = meta.showName;
 
+      // If there's a season number, append it to the state variable
       if (meta.seasonNumber) {
         var state = ` Season ${meta.seasonNumber}`;
+
+        // If there's an episode number, append it to the state variable
         if (meta.episodeNumber) {
           state += ` - Episode ${meta.episodeNumber}`;
         }
       }
 
+      // Try to search for the show and get its image
       try {
         const show = await searchShow(meta.showName);
         var image = show.image;
       } catch {
+        // If the show can't be found, set the image to the VLC logo
         var image = config.iconNames.vlc;
       }
-    } else if (meta.artist) {
-      var state = meta.artist;
-      if (meta.track_number && meta.track_total) {
-        var partySize = parseInt(meta.track_number, 10);
-        var partyMax = parseInt(meta.track_total, 10);
-      }
-      try {
-        var image = await getAlbumArt(meta.album);
-      } catch {
-        var image = config.iconNames.vlc;
-      }
+      // If it's a music video
     } else {
+      // If it's not a TV show but is a video, set the state to the video date (if available) and set the image to the VLC logo
       var state = `${status.date || ""} Video`;
       var image = config.iconNames.vlc;
     }
-  } else if (meta.now_playing) {
-    var state = meta.now_playing || "Stream";
+    // If it's some sort of music file
   } else if (meta.artist) {
     var state = meta.artist;
-
-    if (meta.album) {
-      state += ` - ${meta.album}`;
-      if (meta.track_number && meta.track_total) {
-        var partySize = parseInt(meta.track_number, 10);
-        var partyMax = parseInt(meta.track_total, 10);
-      }
+    // If there's a track number and total number of tracks, set the party size and max
+    if (meta.track_number && meta.track_total) {
+      var partySize = parseInt(meta.track_number, 10);
+      var partyMax = parseInt(meta.track_total, 10);
     }
-
+    // Try to get the album art for the music
     try {
       var image = await getAlbumArt(meta.album);
     } catch {
+      // If the album art can't be found, set the image to rhe VLC logo
       var image = config.iconNames.vlc;
     }
-  } else {
-    var state = status.state;
+    // If the video is currently playing
+  } else if (meta.now_playing) {
+    // Set the state to  the value of the "now_playing" meta data (if available) or "Stream"
+    var state = meta.now_playing || "Stream";
   }
+
+  // Get time left in video
   const end = Math.floor(Date.now() / 1000 + (status.length - status.time) / status.rate);
   if (status.state === "playing" && status.length != 0) {
     var endTimestamp = end;
