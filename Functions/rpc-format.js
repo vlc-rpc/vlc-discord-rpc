92d3d9b69d8e3237e995a961005a19c0056c2ee1
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-04-25 10:54:59 -0400

    Update Functions

--- Functions/rpc-format.js
+++ Functions/rpc-format.js
@@ -1,11 +1,12 @@
 /**
- * Description: Pause or playing?
+ * Description: Decides what information to display based on the nature of the media (video, music, etc)
  */
 
 const { pause } = require("./States/paused.js");
 const { play } = require("./States/playing.js");
 
 module.exports = async (status) => {
+  // Add a pause function so the file does not get too long (makes editing easier as well)
   if (status.state == "paused") {
     var output = pause(status);
   } else {
