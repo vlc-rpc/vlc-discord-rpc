a235b75ff5d7766819a6c6afac05bce2f429a50d
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-04-26 14:26:28 -0400

    Minor performance and clarity updates

--- Functions/client.js
+++ Functions/client.js
@@ -8,9 +8,9 @@ let awake = true;
 let timeInactive = 0;
 
 async function update() {
-  diff(async (status, difference) => {
-    if (difference) {
-      let formatted = await format(status);
+  diff(async (status, shouldUpdate) => {
+    if (shouldUpdate) {
+      const formatted = await format(status);
       client.setActivity(formatted);
       console.log("Presence updated");
 
@@ -26,8 +26,8 @@ async function update() {
           awake = false;
           client.clearActivity();
         } else {
-          let formatted = await format(status);
-          client.setActivity(formatted);
+          const formattedStatus = await format(status);
+          client.setActivity(formattedStatus);
           console.log("Presence updated");
           awake = false;
         }
@@ -40,19 +40,20 @@ client.on("ready", () => {
   console.log("Logged in as", client.user.username);
 });
 
-console.log("Connecting to Discord...");
-client
-  .login({ clientId: config.richPresenseSettings.id })
-  .then(() => {
+async function connectToDiscord() {
+  try {
+    console.log("Connecting to Discord...");
+    await client.login({ clientId: config.richPresenseSettings.id });
     setInterval(update, config.richPresenseSettings.updateInterval);
-  })
-  .catch((err) => {
-    if (err.toString() === "Error: Could not connect") {
+  } catch (error) {
+    if (error.toString() === "Error: Could not connect") {
       console.log("Failed to connect to Discord. Is your Discord client open? Retrying in 20 seconds...");
       // Retry login
-      setTimeout(discordLogin, 20000);
+      setTimeout(connectToDiscord, 20000);
     } else {
       console.log("An unknown error occurred when connecting to Discord");
-      throw err;
+      throw error;
     }
-  });
+  }
+}
+connectToDiscord();
