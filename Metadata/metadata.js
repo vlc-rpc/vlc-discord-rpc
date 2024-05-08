a82ee74e5090970dc0cb7405736ec3f595fad3ef
Toast <61550272+Lauren1066@users.noreply.github.com>
2024-05-08 16:13:30 -0400

    check extension as soon as possible

--- Metadata/metadata.js
+++ Metadata/metadata.js
@@ -12,57 +12,55 @@ async function addMetadata() {
   try {
     // Request input and output file paths from user
     const input_file = await askQuestion(rl, "Enter the input file path: ");
-    let output_file = await askQuestion(rl, "Enter the output file path: ");
+    const extension = input_file.slice(input_file.lastIndexOf('.'));
 
-    while (input_file === output_file) {
-      console.log("Output file must be different from the input file. Please choose a different file path.");
-      output_file = await askQuestion(rl, "Enter the output file path: ");
-    }
+    // Tested extensions. Can add more.
+    const testedExtensions = [".mp4", ".wmv", ".mov", ".mkv"];
+    if(testedExtensions.includes(extension)) {
+      let output_file = await askQuestion(rl, "Enter the output file path: ");
 
-    output_file = await validateFileExtensions(input_file, output_file);
-
-    const cleaned_input_file = input_file.replaceAll('"', '');
-    const cleaned_ouput_file = output_file.replaceAll('"', '');
-
-    /** 
-     * Check if output file exists and handle overwrite scenario. 
-     * If the file doesn't already exist, just use -n (no)
-     */
-    let overwrite = "-n"; 
-    if (fs.existsSync(cleaned_ouput_file)) {
-      overwrite = await handleExistingOutputFile(rl);
-    }
-
-    // Retrieve and validate content type
-    const content_type = await getContentType(rl);
-
-    // Get the name of the show or movie
-    const name = await askQuestion(rl, `Enter the ${content_type} name: `);
-
-    // Execute the ffmpeg command
-    let metadataCommand = `ffmpeg ${overwrite} -i "${cleaned_input_file}" -c copy -metadata title="${name}" -metadata genre="${content_type}"`;
-
-    if (content_type === "show") {
-      const season = await askQuestion(rl, "Enter the season number: ");
-      const episode = await askQuestion(rl, "Enter the episode number: ");
-
-      const extension = cleaned_input_file.slice(cleaned_input_file.lastIndexOf('.'));
-
-      // Tested extensions. Can add more.
-      const testedExtensions = [".mp4", ".wmv", ".mov", ".mkv"];
-
-      if(testedExtensions.includes(extension)) {
-        metadataCommand += ` -metadata comment="S:${season} E:${episode}"`;
-      } else {
-        console.log("That extension has not been tested yet! If you know what you're doing add it on line 56, or join the discord!");
+      while (input_file === output_file) {
+        console.log("Output file must be different from the input file. Please choose a different file path.");
+        output_file = await askQuestion(rl, "Enter the output file path: ");
       }
 
+      output_file = await validateFileExtensions(input_file, output_file);
+
+      const cleaned_input_file = input_file.replaceAll('"', '');
+      const cleaned_ouput_file = output_file.replaceAll('"', '');
+
+      /** 
+       * Check if output file exists and handle overwrite scenario. 
+       * If the file doesn't already exist, just use -n (no)
+       */
+      let overwrite = "-n"; 
+      if (fs.existsSync(cleaned_ouput_file)) {
+        overwrite = await handleExistingOutputFile(rl);
+      }
+
+      // Retrieve and validate content type
+      const content_type = await getContentType(rl);
+
+      // Get the name of the show or movie
+      const name = await askQuestion(rl, `Enter the ${content_type} name: `);
+
+      // Execute the ffmpeg command
+      let metadataCommand = `ffmpeg ${overwrite} -i "${cleaned_input_file}" -c copy -metadata title="${name}" -metadata genre="${content_type}"`;
+
+      if (content_type === "show") {
+        const season = await askQuestion(rl, "Enter the season number: ");
+        const episode = await askQuestion(rl, "Enter the episode number: ");
+
+        metadataCommand += ` -metadata comment="S:${season} E:${episode}"`;
+      }
+
+      metadataCommand += ` "${cleaned_ouput_file}"`;
+      execSync(metadataCommand);
+      console.log("Metadata added successfully.");
+    } else {
+      console.log("That extension has not been tested yet! If you know what you're doing add it on line 18 of metadata.js");
+      console.log("If you don't want to add it yourself join the Discord on the Github or open an Github issue.");
     }
-
-    metadataCommand += ` "${cleaned_ouput_file}"`;
-    execSync(metadataCommand);
-    console.log("Metadata added successfully.");
-
   } catch (error) {
     console.error("An error occurred while adding metadata:", error);
   } finally {
--- Metadata/metadata_folder.js
+++ Metadata/metadata_folder.js
@@ -29,50 +29,49 @@ async function addMetadata() {
         continue;
       }
       const input_file = path.join(directory, file);
+      const extension = input_file.slice(input_file.lastIndexOf('.'));
 
-      const output_file = input_file.substring(0, input_file.lastIndexOf('.')) + "_meta" + input_file.substring(input_file.lastIndexOf('.'));
+      // Tested extensions. Can add more.
+      const testedExtensions = [".mp4", ".wmv", ".mov", ".mkv", ".avi"];
 
-      console.log(`Writing to ${output_file}`);
+      if(testedExtensions.includes(extension)) {
+
+        const output_file = input_file.substring(0, input_file.lastIndexOf('.')) + "_meta" + input_file.substring(input_file.lastIndexOf('.'));
+
+        console.log(`Writing to ${output_file}`);
     
-      /** 
-       * Check if output file exists and handle overwrite scenario. 
-       * If the file doesn't already exist, just use -n (no)
-       */
-      let overwrite = "-n"; 
-      if (fs.existsSync(output_file)) {
-        overwrite = await handleExistingOutputFile();
-      }
-    
-      // Retrieve and validate content type
-      const content_type = await getContentType(rl);
-    
-      // Get the name of the show or movie
-      const name = await askQuestion(`Enter the ${content_type} name: `);
-    
-      // Execute the ffmpeg command
-      let metadataCommand = `ffmpeg ${overwrite} -i "${input_file}" -c copy -metadata title="${name}" -metadata genre="${content_type}"`;
-    
-      if (content_type === "show") {
-        const season = await askQuestion("Enter the season number: ");
-        const episode = await askQuestion("Enter the episode number: ");
-    
-        const extension = input_file.slice(input_file.lastIndexOf('.'));
-    
-        // Tested extensions. Can add more.
-        const testedExtensions = [".mp4", ".wmv", ".mov", ".mkv"];
-    
-        if(testedExtensions.includes(extension)) {
-          metadataCommand += ` -metadata comment="S:${season} E:${episode}"`;
-        } else {
-          console.log("That extension has not been tested yet! If you know what you're doing add it on line 56, or join the discord!");
+        /** 
+         * Check if output file exists and handle overwrite scenario. 
+         * If the file doesn't already exist, just use -n (no)
+         */
+        let overwrite = "-n"; 
+        if (fs.existsSync(output_file)) {
+          overwrite = await handleExistingOutputFile();
         }
     
-      }
+        // Retrieve and validate content type
+        const content_type = await getContentType(rl);
     
-      metadataCommand += ` "${output_file}"`;
-      execSync(metadataCommand);
-      console.log("Metadata added successfully.");
+        // Get the name of the show or movie
+        const name = await askQuestion(`Enter the ${content_type} name: `);
+    
+        // Execute the ffmpeg command
+        let metadataCommand = `ffmpeg ${overwrite} -i "${input_file}" -c copy -metadata title="${name}" -metadata genre="${content_type}"`;
+    
+        if (content_type === "show") {
+          const season = await askQuestion("Enter the season number: ");
+          const episode = await askQuestion("Enter the episode number: ");
 
+          metadataCommand += ` -metadata comment="S:${season} E:${episode}"`;
+        }
+    
+        metadataCommand += ` "${output_file}"`;
+        execSync(metadataCommand);
+        console.log("Metadata added successfully.");
+      } else {
+        console.log("That extension has not been tested yet! If you know what you're doing add it on line 35 of metadata_folder.js");
+        console.log("If you don't want to add it yourself join the Discord on the Github or open an Github issue.");
+      }
     }
   }    catch (error) {
     console.error("An error occurred while adding metadata:", error);
