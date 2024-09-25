83e205d8143cb88bcf56ac79ee41f853be5969d3
Lauren1066 <laurenkeating364@gmail.com>
2024-09-25 12:28:58 -0400

    remove test file

--- test.js
+++
@@ -1,8 +0,0 @@
-async function fetchStuff(showName) {
-  const showURL = `http://api.tvmaze.com/search/shows?q=${showName}`;
-  console.log(showURL);
-  const response = await fetch(showURL);
-  console.log(response);
-}
-
-fetchStuff("Breaking Bad");
